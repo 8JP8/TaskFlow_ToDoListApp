2726f97b772afbd6251e37d4ea924ea01543c29e
João Oliveira <1240369@isep.ipp.pt>
2025-11-03 03:59:32 +0000

    fixes

--- .history/frontend/vue.config_20251102195230.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [],
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: 'all'
-  }
-})
